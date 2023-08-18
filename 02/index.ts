fa3493cd1379cbe48e038ea689cc26ae28ce8c9e
Mateus Caetano <mateus-ramoshd@hotmail.com>
2023-08-18 16:09:08 -0300

    ex2 resolved

--- 02/index.ts
+++ 02/index.ts
@@ -4,4 +4,43 @@ interface User {
   status: boolean;
 }
 
-const findUserByName;
+const findUserByName = (usersList: User[], targetUser: string): User[] => {
+  return usersList.filter((user) =>
+    user.name.toLowerCase().startsWith(targetUser.toLowerCase())
+  );
+};
+
+const usersExample2: User[] = [
+  {
+    name: "Guido",
+    age: 32,
+    status: true,
+  },
+  {
+    name: "Dani",
+    age: 30,
+    status: true,
+  },
+  {
+    name: "João",
+    age: 40,
+    status: false,
+  },
+  {
+    name: "Guilherme",
+    age: 29,
+    status: true,
+  },
+  {
+    name: "Ana",
+    age: 18,
+    status: false,
+  },
+  {
+    name: "José",
+    age: 28,
+    status: false,
+  },
+];
+
+console.log(findUserByName(usersExample2, "Jo"));
